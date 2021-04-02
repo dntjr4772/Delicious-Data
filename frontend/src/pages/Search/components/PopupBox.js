fcb94ffe1548ffcfb649db05768ec6a52b72b2d7
Nanjae <dngngn3045@gmail.com>
2021-04-02 15:59:57 +0900

    [S04P23A405-67] feat : add animation when popup box component is clicked

--- frontend/src/pages/Search/components/PopupBox.js
+++ frontend/src/pages/Search/components/PopupBox.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import styled from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   z-index: 10;
@@ -10,7 +10,13 @@ const Wrapper = styled.div`
   align-items: center;
   height: 100vh;
   width: 100vw;
-  background-color: rgba(200, 200, 200, 0.5);
+  background: rgba(255, 255, 255, 0.75);
+  background-image: radial-gradient(#ffffff 20%, transparent 0),
+    radial-gradient(#ffffff 20%, transparent 0);
+  background-position: 0 0, 5px 5px;
+  background-size: 10px 10px;
+  opacity: ${(props) => (props.isClick ? 1 : 0)};
+  transition: opacity 0.3s;
 `;
 
 const CloseBgDiv = styled.div`
@@ -20,9 +26,21 @@ const CloseBgDiv = styled.div`
   height: 100%;
 `;
 
+const innerFade = keyframes`
+  0%{
+    transform: scale(0, 0);
+  }
+  75%{
+    transform: scale(1.05, 1.05);
+  }
+  100%{
+    transform: scale(1, 1);
+  }
+`;
+
 const Inner = styled.div`
   z-index: 20;
-  width: 640px;
+  width: 620px;
   height: 500px;
   border-radius: 20px;
   border-left: 4px solid #ed8e47;
@@ -33,18 +51,30 @@ const Inner = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  ${(props) =>
+    props.isClick &&
+    css`
+      animation: ${innerFade} 0.3s linear;
+    `};
 `;
 
 let PopupBox = ({ data, dispatch }) => {
+  const [isClick, setIsClick] = useState(false);
+
   const { store_name } = data;
 
   const onClickHandler = () => {
     dispatch({ type: "TOGGLE_POPUP_BOX", dataIndex: -1 });
   };
 
+  useEffect(() => {
+    setIsClick(true);
+    return;
+  }, []);
+
   return (
-    <Wrapper>
-      <Inner>{store_name}</Inner>
+    <Wrapper isClick={isClick}>
+      <Inner isClick={isClick}>{store_name}</Inner>
       <CloseBgDiv onClick={onClickHandler}></CloseBgDiv>
     </Wrapper>
   );
